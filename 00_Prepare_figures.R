b4c08c3c1b4c46ee57d1faab0c6c8d6e5547576c
ampirelar <ampirelar@eafit.edu.co>
2021-11-04 15:34:25 -0500

    subida graficas

---
+++ 00_Prepare_figures.R
@@ -0,0 +1,57 @@
+library(pulsosocialcolombia)
+library(here)
+library(googlesheets4)
+library(tidyverse)
+
+dic_map <-"https://docs.google.com/spreadsheets/d/1ZEqO-bYWPYixr2xI6fNeW4MCEeyZYqFoIRUKllhbeU8/edit?usp=sharing"
+
+#### Get the file
+codvars <-  read_sheet(dic_map, sheet = "Vars", col_names = TRUE)
+
+# for (i in c(320:331)){
+for (i in codvars$var_id) {
+
+  # i <- 309
+  # graph options
+  w <- 7*1.5
+  h <- 4*1.5
+  dpi <- 300
+
+  d <- codvars[codvars$var_id==i,]
+  print(i)
+
+  ### Trend
+  if (d$fig_trend==1) {
+    fl <- paste0(here(),"/img/","var_",d$var_id,"_trend",".png", sep="")
+    try(ggsave(fl,print(pulso_trend(i,"print")),dpi = dpi, width = w, height = h))
+  }
+
+  #### Static
+    fl <- paste0(here(),"/img/","var_",d$var_id,"_static",".png", sep="")
+    try(ggsave(fl,print(pulso_static(i,"print")),dpi = dpi, width = w, height = h))
+
+  #### Map
+    if (d$fig_map==1) {
+      fl <- paste0(here(),"/img/","var_",d$var_id,"_map",".png", sep="")
+      try(ggsave(fl,print(pulso_map(i,"print")),dpi = dpi, width = w, height = h))
+    }
+
+    #### Map change
+    if (d$fig_map==1 & d$fig_trend==1) {
+      fl <- paste0(here(),"/img/","var_",d$var_id,"_map_change",".png", sep="")
+      try(ggsave(fl,print(pulso_map_change(i,"print")),dpi = dpi, width = w, height = h))
+    }
+
+  ### Scatter (level)
+    if(d$fig_scatter==1){
+      fl <- paste0(here(),"/img/","var_",d$var_id,"_scatter",".png", sep="")
+      try(ggsave(fl,print(pulso_scatter(i,"print")),dpi = dpi, width = w, height = h))
+    }
+
+  ### Scatter (time)
+    if (d$fig_scatter_time==1){
+      fl <- paste0(here(),"/img/","var_",d$var_id,"_scatter_time",".png", sep="")
+      try(ggsave(fl,print(pulso_scatter_time(i,"print")),dpi = dpi, width = w, height = h))
+    }
+}
+
